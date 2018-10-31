892b7e71c0a16ce482958012d1be7b1ab36b37c5
Leo Peng <leopengpong@gmail.com>
2018-10-31 00:40:12 -0700

    add Procfile and process.env.PORT

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var socket = require('socket.io');
 
 var app = express();
-var server = app.listen(4000, function () {
+var server = app.listen(process.env.PORT || 4000, function () {
 	console.log('listening to port 4000');
 });
 
